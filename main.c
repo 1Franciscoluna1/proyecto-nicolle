7f1344394119df7922176a3187d2b0795e020ac2
Francisco Luna <pacolunahdz@hotmail.com>
2021-06-05 02:42:50 +0000

    ya jalo

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include<stdio.h>
 //#include<conio.h>
 
-struct dato {
+typedef struct{
 char nombre[30], tel[10];
 int edad, codigo; 	
-}p[10];
-typedef dato d_empleado;
+}dato;
+
 
 int ingreso ()
 {
@@ -28,40 +28,50 @@ void menu()
 	printf("\n\tHamburguesa tocino\n\tburger 77mxn\t\t\tcombo 139mxn");
 	printf("\n\tHamburguesa tocino doble\n\tburger 100mxn\t\t\tcombo  159mxn");
 	printf("\n\tHamburguesa de pollo\n\tburger 92mxn\t\t\tcombo 155mxn");
-	printf("\n\tHamburguesa jalapeño\n\tburger 92mxn\t\t\tcombo 155mxn");
+	printf("\n\tHamburguesa jalapeño\n\tburger 92mxn\t\t\tcombo 155mxn\n\n");
 		
 }
-void usuario()
+void usuario(dato *empleado)
 {
-	//nombre y ID 
-	d_empleado empleado; 
-	int id, telefono[10], edad;
-	char nombre[30];
-	printf("\n\tEscribe el nombre de la persona que esta atendiendo: ");
-	fflush (stdin); gets(empleado.nombre);
-	printf("\n\tEscribe tu numero de empleado:");
-	scanf("%i",& empleado.codigo);
-	printf("\n\tEscribe tu edad: ");
-	scanf("%i",& empleado.edad);
-	printf("\n\tEscribe telefono: ");
-	fflush(stdin); gets(empleado.tel);
+	//nombre y ID  
+	// int id, telefono[10], edad;
+	// char nombre[30];
 
+  for (int i=0;i < 2;i++){
+	printf("\n\tEscribe el nombre de la persona que esta atendiendo: ");
+	fflush (stdin); 
+  scanf(" %[^\n]s",empleado[i].nombre);
+	printf("\n\tEscribe tu numero de empleado:");
+	scanf("%i",& empleado[i].codigo);
+	printf("\n\tEscribe tu edad: ");
+	scanf("%i",& empleado[i].edad);
+	printf("\n\tEscribe telefono: ");
+	fflush(stdin);
+  scanf(" %[^\n]s",empleado[i].tel);
+  }
 	
 }
-void aver()
+void aver(dato *empleado)
 {
+for(int i=0;i < 2;i++){
+  printf("\n\n%s",empleado[i].nombre);
+  printf("\n%s",empleado[i].tel);
+  printf("\n%i",empleado[i].edad);
+  printf("\n%i",empleado[i].codigo);
 
 }
-main()
+}
+int main()
 {
+  dato empleado[2];
 	int me;
-	{
-		me=ingreso();
-		printf("\n\tel numero de mesa es: %i", me);
-	}
+	
+	me=ingreso();
+	printf("\n\tel numero de mesa es: %i", me);
+	
 	menu();
-	usuario();
-	aver();
+	usuario(empleado);
+	aver(empleado);
 	
 	
 }
